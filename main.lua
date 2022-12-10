6fea5a7bd6cfad7b069a26c95f1fbc0343a539c4
Jess Telford <hi+github@jes.st>
2022-12-11 00:54:06 +1100

    Prefix variables depending on their coordinate system

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
 menuitem(1, "debug", function() debugger.expand(true) end)
 
+-- There are 5 coordinate systems, denoted by variable prefixes:
+-- w_*: World. Scaled so 1 world coordinate maps onto 1 screen pixel.
+-- wc_*: World cell. World coordinates divided into 8x8 "cells".
+-- c_*: Camera. PICO8's built-in camera.
+-- mc_*: Map cell. PICO8's built-in map.
+-- s_*: Screen pixels. PICO8's built-in display.
+
 -- Map notes:
 -- We're using the map as a convenient storage space to store the locations of
 -- levels and walls and their associated sprites.
@@ -13,46 +20,47 @@ menuitem(1, "debug", function() debugger.expand(true) end)
 --  32-47 (walls/foreground)
 --
 -- Why 2 screens? So we can incrementally add new cells as old cells scroll off
--- the camera view, then simply render the map at (camy / 8) % 41, instead of
+-- the camera view, then simply render the map at (w_camy / 8) % 41, instead of
 -- having to do huge copy operations each time a cell moves off the screen to
 -- shift the map down.
 
 -- disable button repeat
 poke(0x5f5c, 255)
 
-gravity = -0.2
-jerky = 3.5
+w_gravity = -0.2
+w_jerky = 3.5
 
-inputax = 0.1
+w_inputax = 0.1
 
-airdragx = 0.1
-platformdragx = 0.4
-grounddragx = 0.3
+w_airdragx = 0.1
+w_platformdragx = 0.4
+-- TODO
+w_grounddragx = 0.3
 
-pax = 0
-pay = 0
+w_pax = 0
+w_pay = 0
 
-pvx = 0
-pvy = 0
-maxvx = 5
-maxvy = 10
+w_pvx = 0
+w_pvy = 0
+w_maxvx = 5
+w_maxvy = 10
 
-px = 10
-py = 17
+w_px = 10
+w_py = 17
 
-ph = 16
-pw = 8
+w_ph = 16
+w_pw = 8
+wc_pw = w_pw / 8
 
-camystart = 0
-camy = camystart -- Bottom of the map
-camh = 128
+w_camy = 0 -- Bottom of the map
+w_camh = 128
 
-scrollthreshold = camh * 0.75
-scrollsize = camh - scrollthreshold
+w_scrollthreshold = w_camh * 0.75
+w_scrollsize = w_camh - w_scrollthreshold
 
-scrollspeed = 0
-scrollpushforce = 3
-maxscrollspeed = 40
+w_scrollspeed = 0
+w_scrollpushforce = 3
+w_maxscrollspeed = 40
 
 coll = false
 gameover = false
@@ -61,18 +69,18 @@ highestlevel = 0
 
 -- NOTE: World coordinates are x-positive right, and y-positive up
 -- So we have to convert back to Screen coordinates when rendering
-mapcellsy = 41
-maph = 8 * mapcellsy
+mc_height = 41
+c_maph = 8 * mc_height
 
-screencellswide = 16
+wc_screenwidth = 16
 
-wallcellswide = 1
+wc_wallwidth = 1
 
-towercellswide = screencellswide - (2 * wallcellswide)
+wc_towerwidth = wc_screenwidth - (2 * wc_wallwidth)
 
 -- Must be 2 or more
-levelmincellswide = 4
-levelmaxcellswide = ceil(towercellswide / 2)
+wc_minlevelwidth = 4
+wc_maxlevelwidth = ceil(wc_towerwidth / 2)
 
 -- rndup(0,3) == 0
 -- rndup(1,3) == 3
@@ -85,45 +93,46 @@ function rndup(num, factor)
   return a - a % factor;
 end
 
-function generate_levels(worldcelly1, worldcelly2)
+function generate_levels(wc_y1, wc_y2)
   -- Levels are every 3rd cell, so skip forward to the next cell that is a
   -- multiple of 3
-  local nextlevelcelly = rndup(worldcelly1, 3)
+  local wc_nextlevely = rndup(wc_y1, 3)
 
-  if (nextlevelcelly > worldcelly2) then
+  if (wc_nextlevely > wc_y2) then
     -- Nothing to do
     return
   end
 
-  for worldcelly=nextlevelcelly, worldcelly2, 3 do
-    local level = worldcelly / 3
+  for wc_y=wc_nextlevely, wc_y2, 3 do
+    local level = wc_y / 3
 
     if (level % 100 == 0) then
        -- change the type of level
     end
 
-    local width
-    local levelcellx
+    local wc_levelwidth
+    local wc_levelx
 
     -- Every 50th level is full width
     if (level % 50 == 0) then
-      width = screencellswide
-      levelcellx = 0
+      wc_levelwidth = wc_screenwidth
+      wc_levelx = 0
     else
       -- otherwise it's a random width & position
-      width = levelmincellswide + flr(rnd(levelmaxcellswide - levelmincellswide  + 1))
-      levelcellx = wallcellswide + flr(rnd(towercellswide - width + 1))
+      wc_levelwidth = wc_minlevelwidth + flr(rnd(wc_maxlevelwidth - wc_minlevelwidth  + 1))
+      wc_levelx = wc_wallwidth + flr(rnd(wc_towerwidth - wc_levelwidth + 1))
     end
 
     -- convert from world coords to map coords
-    local mapy = mapcellsy - worldcelly - 1
+    local mc_y = mc_height - (wc_y % mc_height) - 1
 
     -- draw the ends
-    mset(levelcellx, mapy, 1)
-    mset(levelcellx + width - 1, mapy, 3)
+    mset(wc_levelx, mc_y, 1)
+    mset(wc_levelx + wc_levelwidth - 1, mc_y, 3)
+
     -- draw the rest of the level
-    for mapx = levelcellx + 1, levelcellx + width - 2 do
-      mset(mapx, mapy, 2)
+    for mc_x = wc_levelx + 1, wc_levelx + wc_levelwidth - 2 do
+      mset(mc_x, mc_y, 2)
     end
   end
 end
@@ -139,17 +148,18 @@ function _update60()
 
   if (not gameover) then
     if (btn(0)) then
-      pax = -inputax
+      w_pax = -w_inputax
     else if (btn(1)) then
-      pax = inputax
+      w_pax = w_inputax
     else
+      local w_dragx
       if (coll) then
-        dragx = platformdragx
+        w_dragx = w_platformdragx
       else
-        dragx = airdragx
+        w_dragx = w_airdragx
       end
       -- Horizontal velocity always degrades in the opposite direction of motion
-      pax = -sgn(pvx) * min(dragx, abs(pvx))
+      w_pax = -sgn(w_pvx) * min(w_dragx, abs(w_pvx))
     end end
   end
 
@@ -161,52 +171,55 @@ function _update60()
   -- down, resulting in the the player falling back in the direction of
   -- gravity.
   if (not gameover and btn(2) and coll) then
-    pay = max(jerky, abs(pvx) * 1.5)
+    w_pay = max(w_jerky, abs(w_pvx) * 1.5)
   else
     -- Vertical velocity always degrades in the downward direction
-    pay = gravity
+    w_pay = w_gravity
   end
 
-  pvx = mid(-maxvx, pvx + pax, maxvx)
-  pvy = mid(-maxvy, pvy + pay, maxvy)
+  w_pvx = mid(-w_maxvx, w_pvx + w_pax, w_maxvx)
+  w_pvy = mid(-w_maxvy, w_pvy + w_pay, w_maxvy)
 
-  px += pvx
-  py += pvy
+  w_px += w_pvx
+  w_py += w_pvy
 
   if (not gameover) then
-    rely = py - camy
+    local w_rely = w_py - w_camy
 
     -- Fallen through bottom of the screen
-    if (rely < 0) then
+    if (w_rely < 0) then
       gameover = true
       coll = false
     end
 
     -- How fast the screen needs to scroll up
-    scrollforce = mid(
-      scrollspeed,
+    local w_scrollforcey = mid(
+      w_scrollspeed,
       -- The closer to the top of the screen, the faster the screen has to
       -- scroll up
-      ((rely - scrollthreshold) / scrollsize) * scrollpushforce,
+      ((w_rely - w_scrollthreshold) / w_scrollsize) * w_scrollpushforce,
       -- But only to a maximum
-      maxscrollspeed
+      w_maxscrollspeed
     )
 
-    camy += scrollforce
+    w_camy += w_scrollforcey
 
     -- Once past a certain point, the screen starts scrolling automatically
-    if (scrollspeed == 0 and camy > scrollsize) then
-      scrollspeed = 0.2
+    if (w_scrollspeed == 0 and w_camy > w_scrollsize) then
+      w_scrollspeed = 0.2
     end
 
     coll = false
 
     -- Use map data to check for collision with platforms
     -- Only when player is moving downward
-    if (pvy < 0) then
-      mapy = mapcellsy - 1 - flr(py / 8)
-      for mapx = flr(px / 8), flr((px + pw) / 8) do
-        mapspr = mget(mapx, mapy)
+    if (w_pvy < 0) then
+      local wc_y = flr(w_py / 8)
+      local wc_x1 = flr(w_px / 8)
+      local wc_x2 = wc_x1 + wc_pw
+      local mc_y = mc_height - 1 - (wc_y % mc_height)
+      for mc_x = wc_x1, wc_x2 do
+        mapspr = mget(mc_x, mc_y)
         isplatform = fget(mapspr, 0)
 
         if (isplatform) then
@@ -214,26 +227,26 @@ function _update60()
           -- information?
           coll =true
           -- Move back to the top of the platform
-          py += (8 - (py % 8)) - 1
-          pvy = 0
+          w_py += (8 - (w_py % 8)) - 1
+          w_pvy = 0
           -- Floors are rendered every 3rd map tile, hence the division by 3
-          highestlevel = flr(py / 8) / 3
+          highestlevel = flr(w_py / 8) / 3
         end
       end
     end
 
     -- Collision with walls
-    if (px < 2 or px > 126 - pw) then
+    if (w_px < 2 or w_px > 126 - w_pw) then
       -- Hitting the wall adds a bit more drag
-      pvx -= sgn(pvx) * min(airdragx, abs(pvx))
-      pvx = pvx * -1
-      px = mid(0, px, 15 * 8)
+      w_pvx -= sgn(w_pvx) * min(w_airdragx, abs(w_pvx))
+      w_pvx = w_pvx * -1
+      w_px = mid(0, w_px, 15 * 8)
     end
   end
 end
 
 -- Dump to the terminal
---printTable({ pay, pvy }, true)
+--printTable({ w_pay, w_pvy }, true)
 
 function textwidth(str)
   return print(str, 200, 0) - 200
@@ -258,29 +271,29 @@ end
 
 function _draw()
   cls(0)
-  camera(0, maph - camy - camh)
+  camera(0, c_maph - w_camy - w_camh)
   text("icy 8", 64, 115, 14, 2, 0, 'center')
-  map(0, 0, 0, 0, 128, mapcellsy)
-  spr(16, px, maph - py - ph, 1, 2)
+  map(0, 0, 0, 0, 128, mc_height)
+  spr(16, w_px, c_maph - w_py - w_ph, 1, 2)
   camera(0, 0)
   if (gameover) then
     score = highestlevel * 10
-    colwidth = max(textwidth("level "), textwidth(" "..tostr(score)))
-    leftcol = 64 - colwidth
-    rightcol = 64 + colwidth
+    s_colwidth = max(textwidth("level "), textwidth(" "..tostr(score)))
+    s_leftcol = 64 - s_colwidth
+    s_rightcol = 64 + s_colwidth
 
-    texty = 50
+    s_texty = 50
 
-    rectfill(0, texty + 1, 128, texty + 25, 13)
-    rectfill(0, texty + 2, 128, texty + 24, 1)
+    rectfill(0, s_texty + 1, 128, s_texty + 25, 13)
+    rectfill(0, s_texty + 2, 128, s_texty + 24, 1)
 
-    text("game over", 64, texty, 8, 2, 0, 'center')
+    text("game over", 64, s_texty, 8, 2, 0, 'center')
 
-    text("level", leftcol, texty + 12, 12, 1, 0, 'left')
-    text(tostr(highestlevel), rightcol, texty + 12, 12, 1, 0, 'right')
+    text("level", s_leftcol, s_texty + 12, 12, 1, 0, 'left')
+    text(tostr(highestlevel), s_rightcol, s_texty + 12, 12, 1, 0, 'right')
 
-    text("score", leftcol, texty + 21, 10, 4, 0, 'left')
-    text(tostr(score), rightcol, texty + 21, 10, 4, 0, 'right')
+    text("score", s_leftcol, s_texty + 21, 10, 4, 0, 'left')
+    text(tostr(score), s_rightcol, s_texty + 21, 10, 4, 0, 'right')
   end
   --debugger.draw()
   --sdbg()
